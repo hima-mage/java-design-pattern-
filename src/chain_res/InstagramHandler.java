3ab464d742d1df254429f0e2d142dfb0150ac8dc
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-06-02 10:09:16 +0200

    create instagram handler

--- src/chain_res/InstagramHandler.java
+++ src/chain_res/InstagramHandler.java
@@ -1,19 +1,17 @@
 package chain_res;
 
-public class YoutubeHandler implements Handler {
-		
+public class InstagramHandler  implements Handler {
+	
 	Handler handler;
 	
 	@Override
 	public void setNextHandler(Handler handler) {
-		handler = handler;
-		
-	}
-
+		handler = handler; 
+	} 
 	@Override
 	public void handleLink(Video video) {
 		
-		if ( video.getLink().contains("youtube")) {
+		if ( video.getLink().contains("instagram")) {
 			
 			System.out.println( "i can handle it from youtube" );
 		} else { 
@@ -21,5 +19,5 @@ public class YoutubeHandler implements Handler {
 		}
 		
 	}
-	
+
 }
